ffbce82dd92147f7223e3c5cc16ba560a1e45409
sylviot <sylvio.tavares@hotmail.com>
2019-01-18 21:36:51 -0400

    First page auth

---
+++ crystalrain/crystalrain/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace crystalrain
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Google_Clicked(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new Game();
+        }
+    }
+}
